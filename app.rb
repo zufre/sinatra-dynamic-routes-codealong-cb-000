07b42823df8697fe659ffb7fc39de8c3df6ae1e1
Cedric David <cedric.david@btopenworld.com>
2018-09-19 04:19:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,10 +22,7 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     num1 = params[:num1]
     num2 = params[:num2]
-  prod =  num1 * num2
-  prod
+    prod =  num1 * num2
+    prod
   end
-
-
-
 end
